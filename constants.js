d7e390bc4996523bd427106f59c100f54b193206
Vipin Khatri <89641393+khatriji2510@users.noreply.github.com>
2024-03-16 16:19:16 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,39 @@
+// Options the user could type in
+const prompts = [
+  ["jaipur"],
+  ["fortis hospital"],
+  ["apex hospital"],
+  ["delhi"],
+  ["apollo hospital"],
+  ["thank you"],
+  ["yes"],
+  
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["1. Fortis Hospital,   2. Apex Hospital  ,"],
+  ["here is contact info - fortishospital.com     do you want to book bed their?"],
+  ["here is contact info - apexhospital.com     do you want to book bed their?"],
+  ["1. Fortis Hospital,   2. Apollo Hospital  ,"],
+  ["here is contact info - apollohospital.com     do you want to book bed their?"],
+  ["What else i can help you"],
+  ["how many"],
+ 
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
